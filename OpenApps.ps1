7cdb1de8a479eefec99e837f1e19c12c45800c07
Yollie30 <yolandi.vanstaden@yahoo.com>
2024-12-09 10:40:29 +0200

    Add files via upload
    Add PowerShell script for opening apps

---
+++ OpenApps.ps1
@@ -0,0 +1,8 @@
+# Open Outlook
+Start-Process "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
+
+# Open Civil 3D
+Start-Process "C:\Program Files\Autodesk\AutoCAD 2023\acad.exe"
+
+# Open Microsoft Edge
+Start-Process "msedge"
